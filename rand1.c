fbee55f95e97cd86e4f5c91c42dca16c7636312c
Mark <devine38@gmail.com>
2011-05-09 21:36:12 -0700

    Added isWin info to readme Added test input file Added some functions to rand1.c, isWin function is not done, but prints stuff out instead.

--- rand1.c
+++ rand1.c
@@ -41,6 +41,50 @@ void freeboard(void)
 } /* freeboard */
 
 /******************************************************************************\
+* printBoard                                                                *
+\******************************************************************************/
+int printBoard(char* testBoard)
+{
+	int i,j;
+	printf("From input:\n");
+	for (i = 0; i < rows*columns; i++) {
+		printf("%c", testBoard[i]);
+	}
+	printf("\n\n");
+	for (i = rows-1; i > 0; i--) {
+		for (j = 0; j < columns; j++) {
+			printf("%c ", testBoard[i+j*rows]);
+		}
+		printf("\n");
+	}
+
+} /* printBoard */
+
+/******************************************************************************\
+* isWin                                                                 *
+\******************************************************************************/
+int isWin(char* testBoard)
+{
+	int i,j,k,l = 0;
+	int horizontal = rows*(columns-3), vertical = columns*(rows-3), diagonal = 2*(rows-3)*(columns-3);
+	char quads[horizontal+vertical+diagonal][4];
+	printf("Horizontal: %d\nVertical %d\nDiagonal %d\n",horizontal, vertical, diagonal);
+	// Do horizontal sections
+	for (i = rows-1; i > 0; i--) {
+		for (j = 0; j < columns-3; j++) {
+			for (k = 0; k < 4; k++, l++) {
+				quads[l][k]
+				printf("%c ", testBoard[i+(j+k)*rows]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+	// TODO: Do vertical sections
+	// TODO: Do diagonal sections
+} /* isWin */
+
+/******************************************************************************\
 * main                                                                         *
 \******************************************************************************/
 int main(void)
@@ -62,6 +106,9 @@ int main(void)
 		p = 'b';
 	else
 		p = 'g';
+
+	printBoard(board);
+	isWin(board);
 
 	freeboard();
 
